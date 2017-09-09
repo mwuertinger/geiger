e310d9eb3a6f152dad9bb33e909252c68ba1f974
Michael Wuertinger <michael@wuertinger.de>
2017-09-09 12:09:30 +0200

    Further experiment results

--- timediff.go
+++ timediff.go
@@ -8,12 +8,12 @@ import (
 func main() {
 	log.SetFlags(0)
 
-	start, err := time.Parse("2006-01-02 15:04:05", "2017-09-03 13:25:00")
+	start, err := time.Parse("2006-01-02 15:04:05", "2017-09-03 11:49:30")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	end, err := time.Parse("2006-01-02 15:04:05", "2017-09-04 20:14:07")
+	end, err := time.Parse("2006-01-02 15:04:05", "2017-09-03 12:05:24")
 	if err != nil {
 		log.Fatal(err)
 	}
